96da084ba6f3a5ba9027bb9b7e5e4dfe6a6954fe
Izhar-Khan-02358 <kla01012005@example.com>
2025-09-12 06:36:08 +0530

    Watermelon

---
+++ Watermelon.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0 && n!=2){
+        cout<<"YES"<<endl;
+    }else if(n==2){
+        cout<<"NO"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
