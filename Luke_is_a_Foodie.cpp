f2e0a88b56a81fd3bac8dccb043998f200f18265
Izhar-Khan-02358 <kla01012005@example.com>
2025-10-29 18:57:04 +0530

    Luke_is_a_Foodie

---
+++ Luke_is_a_Foodie.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        vector<int>ar(n);
+        for(int i=0;i<n;i++){
+            cin>>ar[i];
+        }
+        vector<pair<int,int>>m(n);
+        for(int i=0;i<n;i++){
+            int l=ar[i]-x;
+            int r=ar[i]+x;
+            m[i]={l,r};
+        }
+
+        int ans=0;
+        int l=m[0].first;
+        int r=m[0].second;
+        for(int i=1;i<n;i++){
+            l=max(l,m[i].first);
+            r=min(r,m[i].second);
+
+            if(l>r){
+                ans++;
+            }
+        }
+
+        cout<<ans<<endl;
+
+    }
+    return 0;
+}
