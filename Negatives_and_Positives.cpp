b9f7c3d40f8aa6258a1d95e1345996c8cbe98e80
Izhar-Khan-02358 <kla01012005@example.com>
2025-12-23 11:14:41 +0530

    Negatives_and_Positives

--- Negatives_and_Positives.cpp
+++ Negatives_and_Positives.cpp
@@ -25,33 +25,10 @@ signed main(){
         }
         sort(br.begin(),br.end());
 
-        int p[n];
-        p[0]=br[0];
-        for(int i=1;i<n;i++){
-            p[i]=br[i]+p[i-1];
-        }
-        
-
-        for(int i=0;i<n;i++){
-            if(ar[i]==0){
-                if(i==0 && ar[i+1]<0){
-                    ni--;
-                }else if(i==n-1 && ar[i-1]<0){
-                    ni--;
-                }else if(ar[i+1]<0 && ar[i-1<0]){
-                    ni-=2;
-                }else if(ar[i+1]<0 && ar[i-1>0]){
-                    ni--;
-                }else if(ar[i+1]>0 && ar[i-1<0]){
-                    ni--;
-                }
-            }
-        }
-
         if(ni%2==0){
             cout<<ps<<endl;
         }else{
-            cout<<ps-(2*(p[0]))<<endl;
+            cout<<ps-(2*(br[0]))<<endl;
         }
     }
     return 0;
