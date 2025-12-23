7e8637af0644cab3f369bea97a01372eef59aec4
Izhar-Khan-02358 <kla01012005@example.com>
2025-12-23 20:25:38 +0530

    Blackslex_and_Password

---
+++ Blackslex_and_Password.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int k,x;
+        cin>>k>>x;
+
+        cout<<k*x+1<<endl;
+    }
+    return 0;
+}
