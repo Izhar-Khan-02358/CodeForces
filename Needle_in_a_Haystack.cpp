a233821dd717faba7d39e9caa63232447618c5d3
Izhar-Khan-02358 <kla01012005@example.com>
2026-02-10 19:19:50 +0530

    Needle_in_a_Haystack

---
+++ Needle_in_a_Haystack.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string a,b;
+        cin>>a;
+        cin>>b;
+        int h=0;
+        map<char,int>am;
+        map<char,int>bm;
+        for(int i=0;i<a.length();i++){
+            am[a[i]]++;
+        }
+        for(int i=0;i<b.length();i++){
+            bm[b[i]]++;
+        }
+
+        for(int i='a';i<='z';i++){
+            if(bm[i]<am[i]){
+                h=1;
+                break;
+            }else{
+                bm[i]-=am[i];
+            }
+        }
+
+        if(h==1){
+            cout<<"Impossible"<<endl;
+            continue;
+        }
+
+        string c="";
+        for(int i='a';i<='z';i++){
+            while(bm[i]--){
+                c+=i;
+            }
+        }
+
+        string d="";
+
+        int i=0;
+        int j=0;
+        while(d.length()<b.length()){
+            if(j==a.length()){
+                d+=c[i++];
+            }else if(i==c.length()){
+                d+=a[j++];
+            }else if(c[i]<a[j]){
+                d+=c[i++];
+            }else{
+                d+=a[j++];
+            }
+        }
+
+        cout<<d<<endl;
+        
+    }
+    return 0;
+}
