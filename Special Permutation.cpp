5b1884b334cbad5166e805053ff84761fc68a1f2
Izhar-Khan-02358 <kla01012005@example.com>
2026-03-17 21:49:23 +0530

    Special Permutation

---
+++ Special Permutation.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        for(int i=2;i<=n;i++){
+            cout<<i<<" ";
+        }
+        cout<<1<<endl;
+        
+    }
+    return 0;
+}
