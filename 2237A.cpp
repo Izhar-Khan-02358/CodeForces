7af3993e02b76e3ba3fcc8b60c8659238d297a8b
Izhar-Khan-02358 <kla01012005@example.com>
2026-06-19 23:24:21 +0530

    2237A

---
+++ 2237A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define endl '\n'
+
+const int N=2*(1e5)+1;
+
+void solve() {
+    int n;
+    cin>>n;
+    int m=0;
+    int f=LLONG_MAX;
+    for(int i=1;i<=n;i++){
+        int u;cin>>u;
+
+        f=min(f,u);
+
+        m+=f;
+    }
+
+    cout<<m<<endl;
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
