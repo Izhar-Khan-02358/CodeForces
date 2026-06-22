b0b64be1e42af5baebc9a2d98ed415433855bc2d
Izhar-Khan-02358 <kla01012005@example.com>
2026-06-23 01:42:05 +0530

    2091D

---
+++ 2091D.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define endl '\n'
+
+const int N=2*(1e5)+1;
+
+void solve() {
+    int n,m,k;
+    cin>>n>>m>>k;
+
+    int r=(k+n-1)/n;
+    int d=m-r;
+    int a=m/(d+1);
+
+    cout<<a<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
