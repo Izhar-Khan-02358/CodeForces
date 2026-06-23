f44747fadf28f0386b264ec15abe4fd82751c6a4
Izhar-Khan-02358 <kla01012005@example.com>
2026-06-24 00:30:53 +0530

    2155A

---
+++ 2155A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define endl '\n'
+
+const int N=2*(1e5)+1;
+
+void solve() {
+    int n;
+    cin>>n;
+
+    cout<<2*(n-1)<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
