308ef681f8e90546bb03424166c66b8717fe1e0a
Izhar-Khan-02358 <kla01012005@example.com>
2026-06-27 01:25:44 +0530

    2201A1

---
+++ 2201A1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define endl '\n'
+
+const int N=2*(1e5)+1;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>ar(n);
+    for(int i=0;i<n;i++){
+        cin>>ar[i];
+    }
+
+    int u=1;
+    int r=ar[0];
+    for(int i=1;i<n;i++){
+        if(ar[i]>r && ar[i]<=ar[i-1]+1){
+            continue;;
+        }else{
+            u++;
+            r=ar[i];
+        }
+    }
+
+    cout<<u<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
