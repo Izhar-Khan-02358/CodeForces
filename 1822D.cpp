0047ce183e62b793082ef5ddf5d17ea7ecf5313b
Izhar-Khan-02358 <iqjajaiz786@gmail.com>
2026-07-12 08:47:00 +0530

    1822D

---
+++ 1822D.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define endl '\n'
+
+const int N=2*(1e5)+1;
+
+void solve() {
+    int n;
+    cin>>n;
+    
+    if(n%2){
+        cout<<-1<<endl;
+    }else{
+        for(int i=0;i<n;i++){
+            if(i%2){
+                cout<<i<<" ";
+            }else{
+                cout<<n-i<<" ";
+            }
+        }cout<<endl;
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
