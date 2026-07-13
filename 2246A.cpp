cd288914d781e4ce4ba87d3fc0e143e1eae22a07
Izhar-Khan-02358 <iqjajaiz786@gmail.com>
2026-07-13 21:35:53 +0530

    2246A

---
+++ 2246A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define endl '\n'
+
+const int N=2*(1e5)+1;
+
+void solve() {
+    int n;
+    cin>>n;
+    
+    for(int i=n;i>=1;i--){
+        cout<<i<<" ";
+    }cout<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
