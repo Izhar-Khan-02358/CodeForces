d722a3a29c18a0b63cead8af75eb5f98f9577ed9
Izhar-Khan-02358 <iqjajaiz786@gmail.com>
2026-07-24 16:09:20 +0530

    8B

---
+++ 8B.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define endl '\n'
+
+const int N=2*(1e5)+1;
+
+void solve() {
+    string s;cin>>s;
+
+    set<pair<int,int>>st;
+
+    int x=0,y=0;
+    st.insert({x,y});
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='L'){
+            x--;
+        }else if(s[i]=='R'){
+            x++;
+        }else if(s[i]=='U'){
+            y++;
+        }else{
+            y--;
+        }
+
+        if(st.count({x,y})){
+            cout<<"BUG"<<endl;
+            return;
+        }
+
+        int h=0;
+        if(st.count({x+1,y})){
+            h++;
+        }
+        if(st.count({x-1,y})){
+            h++;
+        }
+        if(st.count({x,y+1})){
+            h++;
+        }
+        if(st.count({x,y-1})){
+            h++;
+        }
+
+        if(h>1){
+            cout<<"BUG"<<endl;
+            return;
+        }
+    }
+
+    cout<<"OK"<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    
+    return 0;
+}
